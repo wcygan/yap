09115f0efa90b6c73a4ecd1bda7d20209319eafd
Will Cygan <wcygan.io@gmail.com>
2024-04-22 09:41:11 -0500

    it's working :)

--- yap-api/internal/auth/auth.go
+++ yap-api/internal/auth/auth.go
@@ -3,10 +3,42 @@ package auth
 import (
 	"context"
 	auth "github.com/wcygan/yap/generated/go/auth/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/backoff"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/keepalive"
+	"log"
+	"time"
 )
 
 type AuthService struct {
 	auth.UnimplementedAuthServiceServer
+}
+
+var authClient auth.AuthServiceClient
+
+func init() {
+	conn, err := grpc.Dial("auth-service:50051",
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithKeepaliveParams(keepalive.ClientParameters{
+			Time:                10 * time.Second,
+			Timeout:             5 * time.Second,
+			PermitWithoutStream: true,
+		}),
+		grpc.WithConnectParams(grpc.ConnectParams{
+			Backoff: backoff.Config{
+				BaseDelay:  1.0 * time.Second,
+				Multiplier: 1.5,
+				Jitter:     0.2,
+				MaxDelay:   60 * time.Second,
+			},
+			MinConnectTimeout: time.Second * 10,
+		}),
+	)
+	if err != nil {
+		log.Fatalf("failed to connect to auth service: %v", err)
+	}
+	authClient = auth.NewAuthServiceClient(conn)
 }
 
 func NewAuthService() *AuthService {
@@ -14,27 +46,17 @@ func NewAuthService() *AuthService {
 }
 
 func (s *AuthService) Register(ctx context.Context, req *auth.RegisterRequest) (*auth.RegisterResponse, error) {
-	return &auth.RegisterResponse{
-		AccessToken:  "test",
-		RefreshToken: "test",
-	}, nil
+	return authClient.Register(ctx, req)
 }
 
 func (s *AuthService) Login(ctx context.Context, req *auth.LoginRequest) (*auth.LoginResponse, error) {
-	return &auth.LoginResponse{
-		AccessToken:  "test",
-		RefreshToken: "test",
-	}, nil
+	return authClient.Login(ctx, req)
 }
 
 func (s *AuthService) Validate(ctx context.Context, req *auth.ValidateRequest) (*auth.ValidateResponse, error) {
-	return &auth.ValidateResponse{
-		Username: "test",
-	}, nil
+	return authClient.Validate(ctx, req)
 }
 
 func (s *AuthService) Refresh(ctx context.Context, req *auth.RefreshRequest) (*auth.RefreshResponse, error) {
-	return &auth.RefreshResponse{
-		AccessToken: "test",
-	}, nil
+	return authClient.Refresh(ctx, req)
 }
