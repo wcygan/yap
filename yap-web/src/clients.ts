9321938176effb6ca70b133b60dc5465b3f1b00e
Will Cygan <wcygan.io@gmail.com>
2024-06-13 00:00:16 -0500

    nit

--- yap-web/src/clients.ts
+++ yap-web/src/clients.ts
@@ -1,10 +1,10 @@
 import {createConnectTransport} from "@bufbuild/connect-web";
 import {createPromiseClient} from "@bufbuild/connect";
-import {ChatRoomService, MessagingService} from "./generated/chat/v1/chat_connectweb"
-import {AuthService} from "./generated/auth/v1/auth_connectweb"
+import {ChatRoomService, MessagingService} from "./generated/chat/v1/chat_connectweb";
+import {AuthService} from "./generated/auth/v1/auth_connectweb";
 
 const transport = createConnectTransport({
-    baseUrl: "http://localhost:50050",
+  baseUrl: "https://localhost:50050",
 });
 
 const messagingClient = createPromiseClient(MessagingService, transport);
