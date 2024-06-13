e33b3a12333f6a0883cdd5a65472ca674691952d
Will Cygan <wcygan.io@gmail.com>
2024-06-13 01:06:21 -0500

    add some breadcrumbs for later... need to get envoy running in k8s

--- yap-web/src/clients.ts
+++ yap-web/src/clients.ts
@@ -1,14 +1,18 @@
-import {createConnectTransport} from "@bufbuild/connect-web";
+import {createConnectTransport, createGrpcWebTransport} from "@bufbuild/connect-web";
 import {createPromiseClient} from "@bufbuild/connect";
 import {ChatRoomService, MessagingService} from "./generated/chat/v1/chat_connectweb";
 import {AuthService} from "./generated/auth/v1/auth_connectweb";
 
-const transport = createConnectTransport({
-  baseUrl: "http://yap-api:50050",
+const grpcWebTransport = createGrpcWebTransport({
+    baseUrl: "http://yap-api:50050",
 });
 
-const messagingClient = createPromiseClient(MessagingService, transport);
-const chatRoomClient = createPromiseClient(ChatRoomService, transport);
-const authClient = createPromiseClient(AuthService, transport);
+const connectTransport = createConnectTransport({
+    baseUrl: "http://yap-api:50050",
+});
 
-export { messagingClient, chatRoomClient, authClient };
+const messagingClient = createPromiseClient(MessagingService, grpcWebTransport);
+const chatRoomClient = createPromiseClient(ChatRoomService, grpcWebTransport);
+const authClient = createPromiseClient(AuthService, grpcWebTransport);
+
+export {messagingClient, chatRoomClient, authClient};
